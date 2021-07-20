a0119e5a0f47d3467b530c44bb29ff71b9919d71
Jochen Wezel <jwezel@compumaster.de>
2021-07-20 21:34:05 +0200

    Multi targetting frameworks (#1)
    * add multi-targetting-framework projects
    
    * finalize milestone
    
    Co-authored-by: Thomas Schädler <tschaedler@compuamster.de>

--- nuget-build-and-push.cmd
+++
@@ -1,3 +0,0 @@
-::REM -UpdateNuGetExecutable not required since it's updated by VS.NET mechanisms
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'Scopevisio.Teamwork\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\Scopevisio.Teamwork\CompuMaster.Scopevisio.Teamwork.csproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
-pause
